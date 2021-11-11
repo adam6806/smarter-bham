97da0a361907004138cd0959787b758faec1495a
adam6806 <asmith0935@gmail.com>
2021-11-11 16:38:25 -0600

    Refactored to new models

--- src/main/java/com/smarterbham/api/model/Device.java
+++ src/main/java/com/smarterbham/api/model/Device.java
@@ -6,8 +6,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
@@ -17,7 +15,6 @@ import javax.persistence.Id;
 public class Device {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String deviceId;
     private Double latitude;
     private Double longitude;
