9deb1746e0014f563df4a024fad65040ff96b147
adam6806 <asmith0935@gmail.com>
2021-11-16 10:56:02 -0600

    Fixed DTO

--- src/main/java/com/smarterbham/api/dto/RxMetadatum.java
+++ src/main/java/com/smarterbham/api/dto/RxMetadatum.java
@@ -17,7 +17,6 @@ public class RxMetadatum implements Serializable {
     private final static long serialVersionUID = -8332398093512070497L;
     public GatewayIds gatewayIds;
     public String time;
-    public Integer timestamp;
     public Integer rssi;
     public Integer channelRssi;
     public Double snr;
